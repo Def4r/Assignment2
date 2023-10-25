1148725e6676a4139bb7339762a18e1a568acefe
Daniel <you@example.com>
2023-10-25 18:12:50 -0400

    Added the user check to the method testing it

--- Product.py
+++ Product.py
@@ -1,6 +1,8 @@
 import random
 
 class ProductClass:
+
+    NoLimit = float('inf')
     
     def __init__(self, productName = "", productCode = 0, productSalesPrice = 0, manufactorCost = 0, stockLevel = 0, estManufactoredMnthly = 0):
         self.__productName = productName
@@ -16,14 +18,18 @@ class ProductClass:
                 num = int(input(inputP))
                 if MinInt <= num <= MaxInt:
                     return num
+                else:
+                    print("Input must be between ", MinInt, " and ", MaxInt, "Try Again.")
+            except ValueError:
+                print("Input recieved is not vaild. Try again.")
 
     def BusinessInfo(self):
         self.__productName = input("Whats is the name of your product? :>")
-        self.__productCode = int(input("What is the code for your product? :>"))
-        self.__productSalesPrice = int(input("How much would you price your products? CAD ONLY :>"))
-        self.__manufactorCost = int(input("How much does it cost to make your product? :>"))
-        self.__stockLevel = int(input("How much do you currently have in your stock?"))
-        self.__estManufactoredMnthly = int(input("How much do you manufactor every month. This is an Estimate. :>"))
+        self.__productCode = userVaild("What is the code for your product? :>", 100, 1000)
+        self.__productSalesPrice = userVaild("How much would you price your products? CAD ONLY :>", 0, NoLimit)
+        self.__manufactorCost = userVaild("How much does it cost to make your product? :>", 0, NoLimit)
+        self.__stockLevel = userVaild("How much do you currently have in your stock?", 0, NoLimit)
+        self.__estManufactoredMnthly = userVaild("How much do you manufactor every month. This is an Estimate. :>", 0, NoLimit)
 
     def salesEachMonth(self, salesMonthly):
         randomInc = random.randint(-10 , 10)
