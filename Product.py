8b46d0fecf52ae081bfeefa62c948b97a0a1e465
Daniel <you@example.com>
2023-10-25 14:28:36 -0400

    added init method into class

---
+++ Product.py
@@ -0,0 +1,6 @@
+class ProductClass:
+    
+    def __init__(self, productName = "", productCode = 0, productSales = 0):
+        self.__productName = productName
+        self.__productCode = productCode
+        self.__productSales = productSales
