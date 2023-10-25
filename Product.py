e7e9f533ea812a0233ee6b78cce387596272a4ad
Daniel <you@example.com>
2023-10-25 15:45:55 -0400

    added method to calucate the random sales monthly

--- Product.py
+++ Product.py
@@ -1,3 +1,5 @@
+import random
+
 class ProductClass:
     
     def __init__(self, productName = "", productCode = 0, productSalesPrice = 0, manufactorCost = 0, stockLevel = 0, estManufactoredMnthly = 0):
@@ -7,3 +9,12 @@ class ProductClass:
         self.__manufactorCost = manufactorCost
         self.__stockLevel = stockLevel
         self.__estManufactoredMnthly = estManufactoredMnthly
+
+    def salesEachMonth(self, salesMonthly):
+        randomInc = random.randint(-10 , 10)
+        salesMonthly = self.__estManufactoredMnthly + randomInc
+        return salesMonthly
+
+        
+
+
