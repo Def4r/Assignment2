18c5777e457d5de24e0311b6956a87839d817703
Daniel <you@example.com>
2023-10-25 17:14:54 -0400

    Added first piece of user interaction

--- Product.py
+++ Product.py
@@ -10,10 +10,20 @@ class ProductClass:
         self.__stockLevel = stockLevel
         self.__estManufactoredMnthly = estManufactoredMnthly
 
+    def BusinessInfo(self):
+        self.__productName = input("Whats is the name of your product? :>")
+        self.__productCode = int(input("What is the code for your product? :>"))
+        self.__productSalesPrice = int(input("How much would you price your products? CAD ONLY :>"))
+        self.__manufactorCost = int(input("How much does it cost to make your product? :>"))
+        self.__stockLevel = int(input("How much do you currently have in your stock?"))
+        self.__estManufactoredMnthly = int(input("How much do you manufactor every month. This is an Estimate. :>"))
+
     def salesEachMonth(self, salesMonthly):
         randomInc = random.randint(-10 , 10)
         salesMonthly = self.__estManufactoredMnthly + randomInc
         return salesMonthly
+
+    
 
         
 
---
+++ RunBusiness.py
@@ -0,0 +1,8 @@
+from Product import ProductClass
+
+print("***Welcome to the Business Production Sim***")
+
+Business = ProductClass()
+
+
+
