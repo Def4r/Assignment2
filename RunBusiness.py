88684798efa0be5a44a4633d11cc31c624bea03f
Daniel <you@example.com>
2023-10-26 14:25:30 -0400

    Seem to have fixed Remaining stock

--- RunBusiness.py
+++ RunBusiness.py
@@ -9,19 +9,28 @@ Business = ProductClass()
 Business.BusinessInfo()
 Business.DisplayBusinessInfo()
 
+print("")
+sleep(1)
+print("***Business in year statement***")
+print("")
+
 NetProfits = 0
 Stock = Business.getStockLvl()
+CurrentStock = Business.getStockLvl()
 for months in range(1, 13):
     sleep(2)
     sales = random.randint(-10 , 10)
     print("Month",months, ":")
     print("Manufactored: ",Business.getEstManufactored())
     print("Sold ",Business.salesEachMonth(sales))
-    print("Stock Left:", Business.remainingStock(sales, Stock))
+    CurrentStock -= sales
+    print("Stock Left:", CurrentStock)
     print("")
-    MonthlyProfits = (Business.salesEachMonth(sales) * Business.getProductPrice()) - Business.getManufactorCost()
+    MonthlyRev = (Business.salesEachMonth(sales) * Business.getProductPrice()) 
+    MonthlyCost = (Business.salesEachMonth(sales) * Business.getManufactorCost()) 
+    MonthlyProfits = MonthlyRev - MonthlyCost
     NetProfits += MonthlyProfits
-    
+
 print("Net Profits over 12 Months : $", NetProfits)
 
 
